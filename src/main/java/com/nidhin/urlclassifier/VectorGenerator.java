84a8ebaaf6b9b49ed0fa1c747755430b10bbd3a8
Nidhin <nidhin_t@rocketmail.com>
2017-10-08 01:09:26 +0530

    minor fix in vector generator

--- src/main/java/com/nidhin/urlclassifier/VectorGenerator.java
+++ src/main/java/com/nidhin/urlclassifier/VectorGenerator.java
@@ -5,9 +5,9 @@ import de.l3s.boilerpipe.BoilerpipeProcessingException;
 import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 /**
  * Created by nidhin on 20/7/17.
@@ -16,9 +16,11 @@ public class VectorGenerator {
     HashMap<String, ArrayList<String>> aspirationUrlsMap = new HashMap<>();
     HashMap<String, ArrayList<String>> urlKeywordsMap = new HashMap<>();
     HashMap<String, ArrayList<String>> urlMetaKeywordsMap = new HashMap<>();
-    HashMap<String, double[]> keyVectorMap = new HashMap<>();
+//    HashMap<String, double[]> keyVectorMap = new HashMap<>();
     HashMap<String, HashSet<String>> aspirationKeywordsMap = new HashMap<>();
     HashMap<String, HashSet<double[]>> aspirationKeyVectorsMap = new HashMap<>();
+    HashMap<String, WordKmeans.Classes[]> urlClustersMap = new HashMap<>();
+
 
     public void generate(String [] files) throws IOException, BoilerpipeProcessingException {
 
@@ -40,51 +42,78 @@ public class VectorGenerator {
         Word2Vecdl4j word2Vec = new Word2Vecdl4j();
         word2Vec.loadModel("en", new File("/home/ubuntu/aspirations-oct2nd/GoogleNews-vectors-negative300.bin"));
 
-        int aspCount =0;
-        for (String key : aspirationUrlsMap.keySet()){
+        AtomicInteger aspCount = new AtomicInteger(0);
+        for (String key : aspirationUrlsMap.keySet()) {
             aspirationKeywordsMap.put(key, new HashSet<>());
             System.out.println("Fetching keys for - " + key);
-            aspCount++;
-            int count = 0;
+            aspCount.incrementAndGet();
+            AtomicInteger count = new AtomicInteger(0);
             int totUrls = aspirationUrlsMap.get(key).size();
-            for (String url : aspirationUrlsMap.get(key)){
-                try{
-                    if (url.contains("pdf") || url.contains("PDF")){
-                        System.out.println("SKIPPING PDF");
-                        continue;
-                    }
-                    count++;
-                    if (!keywordExtractor.loadHtml(url, key)) {
-                        keywordExtractor.generateHtmlDoc(url);
-                        keywordExtractor.saveHtmlAsFile("htmls", key);
-                    }
+//            for (String url : aspirationUrlsMap.get(key)){
+            List<HashMap<String, WordKmeans.Classes[]>> urlmaps = aspirationUrlsMap.get(key).stream()
+                    .map(url -> {
+                        try {
+                            if (url.contains("pdf") || url.contains("PDF")) {
+                                System.out.println("SKIPPING PDF");
+                                return null;
+                            }
+                            count.incrementAndGet();
+                            if (!keywordExtractor.loadHtml(url, key)) {
+                                keywordExtractor.generateHtmlDoc(url);
+                                keywordExtractor.saveHtmlAsFile("htmls", key);
+                            }
 //                    keywordExtractor.loadHtml(url, key);
-                    HashSet<String> keywords = keywordExtractor.extractKeyWords();
-                    HashSet<String> metaKeywords = keywordExtractor.getStrictMetaKeywords();
-                    if (count % 50 == 0){
-                        System.out.println("fetched url count - " + count + "/" + totUrls + " asp count - " + aspCount);
-                    }
-                    urlKeywordsMap.put(url, new ArrayList<>(keywords));
-                    urlMetaKeywordsMap.put(url, new ArrayList<>(metaKeywords));
-                    aspirationKeywordsMap.get(key).addAll(keywords);
-                    for (String keyword : keywords){
-                        if (keyVectorMap.containsKey(keyword))
-                            continue;
-                        double[] keyVector = word2Vec.getVectorForNGram("en", keyword);
-                        keyVectorMap.put(keyword, keyVector);
-                    }
-                    for (String keyword : metaKeywords){
-                        if (keyVectorMap.containsKey(keyword))
-                            continue;
-                        double[] keyVector = word2Vec.getVectorForNGram("en", keyword);
-                        keyVectorMap.put(keyword, keyVector);
-                    }
-                }catch (StackOverflowError e){
-                    e.printStackTrace();
-                }
+                            HashSet<String> keywords = keywordExtractor.extractKeyWords();
+                            HashSet<String> metaKeywords = keywordExtractor.getStrictMetaKeywords();
+                            if (count.get() % 50 == 0) {
+                                System.out.println("fetched url count - " + count + "/" + totUrls + " asp count - " + aspCount.get());
+                            }
+                            urlKeywordsMap.put(url, new ArrayList<>(keywords));
+                            urlMetaKeywordsMap.put(url, new ArrayList<>(metaKeywords));
+                            aspirationKeywordsMap.get(key).addAll(keywords);
 
+                            HashSet<String> keySet = new HashSet<>(metaKeywords);
+                            keySet.addAll(keywords);
+                            HashMap<String, double[]> keyWordsDoubleVecMap = new HashMap<>();
+                            int totalKwords = 0;
+                            for (String keyword : keySet) {
+                                double[] vec = word2Vec.getVectorForNGram("en", keyword.trim().toLowerCase());
+                                if (vec != null) {
+                                    keyWordsDoubleVecMap.put(keyword, vec);
+                                    if (totalKwords > 5000) {
+                                        break;
+                                    }
+                                    totalKwords++;
+                                }
+
+                            }
+                            System.out.println("clustering...  :: Keywords count - " + keyWordsDoubleVecMap.size());
+                            WordKmeans wordKmeans = new WordKmeans(keyWordsDoubleVecMap, 50, 100);
+                            WordKmeans.Classes[] classes = wordKmeans.explain();
+//                    urlClustersMap.put(url, classes);
+                            System.out.println("classes - " + classes.length + " url - " + url);
+                            HashMap<String, WordKmeans.Classes[]> map = new HashMap<>();
+                            map.put(url, classes);
+                            return map;
+                        } catch (StackOverflowError e) {
+                            e.printStackTrace();
+                        } catch (BoilerpipeProcessingException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        return null;
+                    })
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toList());
+
+            for (HashMap<String, WordKmeans.Classes[]> map : urlmaps){
+                urlClustersMap.putAll(map);
             }
+
         }
+
 
         System.out.println("serializing...");
         ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("aspirationUrlsMap.ser"));
@@ -100,12 +129,19 @@ public class VectorGenerator {
         oos.writeObject(urlMetaKeywordsMap);
         oos.flush();
         oos.close();
-        oos = new ObjectOutputStream(new FileOutputStream("keyVectorMap.ser"));
-        oos.writeObject(keyVectorMap);
-        oos.flush();
-        oos.close();
+//        oos = new ObjectOutputStream(new FileOutputStream("keyVectorMap.ser"));
+//        oos.writeObject(keyVectorMap);
+//        oos.flush();
+//        oos.close();
         oos = new ObjectOutputStream(new FileOutputStream("aspirationKeywordsMap.ser"));
         oos.writeObject(aspirationKeywordsMap);
+        oos.flush();
+        oos.close();
+
+
+
+        oos = new ObjectOutputStream(new FileOutputStream("urlClustersMap-google.ser"));
+        oos.writeObject(urlClustersMap);
         oos.flush();
         oos.close();
 
